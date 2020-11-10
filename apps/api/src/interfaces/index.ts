303b5385d3cd1d30c1812e70e1157271d5a31a62
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-11-10 14:18:53 +0100

    feat(api/model): add ITurn interface and Turn class

--- apps/api/src/interfaces/index.ts
+++ apps/api/src/interfaces/index.ts
@@ -2,3 +2,5 @@ export type { IPlayerType, IPlayer, IHUMAN, IAI } from './player.interface'
 export type { IMatchState } from './match-state.interface'
 
 export { MatchState } from './match-state.interface'
+
+export type { ITurn } from './turn.interface'
---
+++ apps/api/src/interfaces/turn.interface.ts
@@ -0,0 +1,39 @@
+import type { IPlayer } from './player.interface'
+
+export interface ITurn<Player1 extends IPlayer, Player2 extends IPlayer> {
+  /**
+   * initialize the state by
+   * - setting the currentTurn to Player1
+   * - setting the turnNumber to 1
+   * @memberof ITurn
+   */
+  init(): void
+
+  /**
+   * gets the number of the current turn
+   * @returns {number}
+   * @memberof ITurn
+   */
+  getTurnNumber(): number
+
+  /**
+   * gets who is playing the current turn
+   * @returns {(Player1 | Player2)}
+   * @memberof ITurn
+   */
+  getCurrent(): Player1 | Player2
+
+  /**
+   * gets who will be playing the next turn (without setting it)
+   * @returns {(Player1 | Player2)}
+   * @memberof ITurn
+   */
+  peekNext(): Player1 | Player2
+
+  /**
+   * switch turn and then returns who's turn is
+   * @returns {(Player1 | Player2)}
+   * @memberof ITurn
+   */
+  next(): Player1 | Player2
+}
