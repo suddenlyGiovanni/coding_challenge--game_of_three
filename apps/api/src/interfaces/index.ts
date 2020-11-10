1400dcbd38e24b5bb9a1bf5d49852f157163a441
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-11-10 13:20:27 +0100

    feat(api/model): add IMatchState interface

--- apps/api/src/interfaces/index.ts
+++ apps/api/src/interfaces/index.ts
@@ -1,1 +1,4 @@
 export type { IPlayerType, IPlayer, IHUMAN, IAI } from './player.interface'
+export type { IMatchState } from './match-state.interface'
+
+export { MatchState } from './match-state.interface'
