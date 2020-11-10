5fa0b2f2483b74e9145d848e08c1b7e488c2e693
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-11-10 17:51:03 +0100

    feat(api/model): sketching ILobby interface

--- apps/api/src/interfaces/index.ts
+++ apps/api/src/interfaces/index.ts
@@ -1,6 +1,7 @@
 export type { IPlayerType, IPlayer, IHUMAN, IAI } from './player.interface'
 export type { IMatchState } from './match-state.interface'
 export type { ITurn } from './turn.interface'
+export type { ILobby } from './lobby.interface'
 
 export { MatchState } from './match-state.interface'
 export { PlayerType } from './player.interface'
---
+++ apps/api/src/interfaces/lobby.interface.ts
@@ -0,0 +1,37 @@
+/*
+- there could only be one instance of a Lobby at the time.
+- is responsible for registering connected users
+- should implement the following operations: add user, remove user, start a match
+*/
+
+import type { Human } from '../model/human'
+
+export interface ILobby {
+  /**
+   * gets an array representation of all the human IPlayers currently in the ILobby
+   * @returns {ReadonlyArray<Human>}
+   * @memberof ILobby
+   */
+  getPlayers(): ReadonlyArray<Human>
+
+  /**
+   * gets the number of human IPlayer currently in the ILobby
+   * @returns {number}
+   * @memberof ILobby
+   */
+  getSize(): number
+
+  /**
+   * adds a human IPlayer to the ILobby
+   * @param {Human} player
+   * @memberof ILobby
+   */
+  addPlayer(player: Human): void
+
+  /**
+   * removes a human IPlayer from the ILobby
+   * @param {Human} player
+   * @memberof ILobby
+   */
+  removePlayer(player: Human): void
+}
