306b6c6e30ac9e7b8c787aa67d1189bfe0b99508
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-11-10 14:18:53 +0100

    feat(api/model): add ITurn interface and Turn class

---
+++ apps/api/src/model/turn.ts
@@ -0,0 +1,54 @@
+import type { IPlayer, ITurn } from '../interfaces'
+
+export class Turn<Player1 extends IPlayer, Player2 extends IPlayer>
+  implements ITurn<Player1, Player2> {
+  private readonly player1: Player1
+  private readonly player2: Player2
+  private turnNumber: number
+  private currentTurn: Player1 | Player2
+
+  public constructor(player1: Player1, player2: Player2) {
+    this.player1 = player1
+    this.player2 = player2
+  }
+
+  public init(): void {
+    this.currentTurn = this.player1
+    this.turnNumber = 1
+  }
+
+  private assertInt(): void {
+    if (this.turnNumber === undefined || !this.currentTurn) {
+      throw new Error(
+        'Turn not initialized: remember to call init() on a newly instantiated Turn'
+      )
+    }
+  }
+
+  public getTurnNumber(): number {
+    this.assertInt()
+    return this.turnNumber
+  }
+
+  public getCurrent(): Player1 | Player2 {
+    this.assertInt()
+    return this.currentTurn
+  }
+
+  public peekNext(): Player1 | Player2 {
+    this.assertInt()
+    if (this.currentTurn.isSame(this.player1)) {
+      return this.player2
+    }
+    if (this.currentTurn.isSame(this.player2)) {
+      return this.player1
+    }
+  }
+
+  public next(): Player1 | Player2 {
+    this.assertInt()
+    this.turnNumber += 1
+    this.currentTurn = this.peekNext()
+    return this.currentTurn
+  }
+}
