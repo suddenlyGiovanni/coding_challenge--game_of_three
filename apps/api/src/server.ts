93629716bb23e917882e57dfd8b23a5d409ea43a
Giovanni Ravalico <ravalico.giovanni@gmail.com>
2020-11-20 14:16:22 +0100

    refactor(api|client|api-interfaces): move interface definitions to api-interfaces

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -2,124 +2,14 @@ import { Server as IOServer, Socket } from 'socket.io'
 
 import type { IPlayersStore, IServer } from './interfaces'
 import { Human } from './model'
-
 import { PlayersStore } from './players-store'
+
+import { ISocketEvent, SocketEvent } from '@game-of-three/api-interfaces'
 
 export type SocketActionFn<T> = (socket: Socket) => (message: T) => void
 export interface WrappedServerSocket<T> {
   callback: SocketActionFn<T>
   event: string
-}
-
-export enum SocketEvent {
-  //#region SYSTEM RESERVED EVENTS
-
-  /**
-   * Fired upon a connection from client.
-   * socket (Socket) socket connection with client
-   * @example
-   * ```
-   * io.on('connection', (socket) => {
-   *  // ...
-   * });
-   *
-   * io.of('/admin').on('connection', (socket) => {
-   *  // ...
-   * });
-   * ```
-   */
-  CONNECT = 'connect',
-  /**
-   * Synonym of Event: ‘connect’.
-   */
-  CONNECTION = 'connection',
-  /**
-   * Fired upon disconnection
-   * reason (String) the reason of the disconnection (either client or server-side):
-   *
-   * | `transport error`
-   * | `server namespace disconnect`
-   * | `client namespace disconnect`
-   * | `ping timeout`
-   * | `transport close`
-   * @example
-   * ```
-   * io.on('connection', (socket) => {
-   *  socket.on('disconnect', (reason) => {
-   *    // ...
-   *   });
-   * });
-   * ```
-   */
-  DISCONNECT = 'disconnect',
-
-  /**
-   * Fired when the client is going to be disconnected (but hasn’t left its rooms yet).
-   * reason (String) the reason of the disconnection (either client or server-side)
-   * @example
-   * ```
-   * io.on('connection', (socket) => {
-   *  socket.on('disconnecting', (reason) => {
-   *    console.log(socket.rooms); // Set { ... }
-   *  });
-   * });
-   * ```
-   */
-  DISCONNECTING = 'disconnecting',
-
-  /**
-   * Fired upon a connection error
-   */
-  CONNECT_ERROR = 'connect_error',
-
-  /**
-   * Fired upon a connection timeout
-   */
-  CONNECT_TIMEOUT = 'connect_timeout',
-
-  /**
-   * Fired upon an attempt to reconnect
-   */
-  RECONNECT_ATTEMPT = 'reconnect_attempt',
-
-  /**
-   * Fired upon a reconnection attempt error
-   */
-  reconnect_error = 'reconnect_error',
-
-  /**
-   * Fired when the client couldn’t reconnect within reconnectionAttempts
-   */
-  RECONNECT_FAILED = 'reconnect_failed',
-
-  /**
-   * Alias for “reconnect_attempt”
-   */
-  RECONNECTING = 'reconnecting',
-
-  /**
-   * Fired upon a successful reconnection
-   */
-  RECONNECT = 'reconnect',
-
-  /**
-   * Fired when a `ping` is sent to the server
-   */
-  PING = 'ping',
-
-  /**
-   * Fired when a `pong` is received from the server
-   */
-  PONG = 'pong',
-
-  NEW_LISTENER = 'newListener',
-
-  REMOVE_LISTENER = 'removeListener',
-
-  //#endregion SYSTEM RESERVED EVENTS
-
-  HEARTBEAT = 'heartbeat',
-  HELLO = 'hello',
 }
 
 export class Server implements IServer {
@@ -166,7 +56,7 @@ export class Server implements IServer {
   }
 
   private _createSocket<T>(
-    event: SocketEvent,
+    event: ISocketEvent,
     action?: SocketActionFn<T>
   ): WrappedServerSocket<T> {
     return { callback: action, event }
